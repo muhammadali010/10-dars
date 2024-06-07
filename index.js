fb7219d742a38ea2e110ad8d03320e97ac755ef0
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-06-07 18:23:43 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,240 @@
+// 1chi masala
+// let arr = [13, 25, 32, 36, 12];
+// let sum = [];
+// let res = arr.forEach(function (value,) {
+//     sum.push(value * value);
+// });
+// console.log(sum);
+
+
+// 2chi masala
+// let arr = [-13, 25, -32, 26, 12];
+// let massiv = [];
+// arr.forEach(function (value) {
+//     if (value > 0) {
+//         massiv.push(value)
+//     }
+// })
+// console.log(massiv);
+
+
+// 3chi masala
+// let arr = [13, 25, 32, 16, 12];
+// arr.forEach(function (value) {
+//     console.log(value * 2);
+// })
+
+
+// 4chi masala
+// let arr = [13, 25, 32, 41, 12];
+// let sum = 0;
+// arr.forEach(function (value) {
+//     sum = sum + value;
+// })
+// console.log(sum);
+
+
+// 5chi masala
+// let arr = [-13, 25, -32, 14, -12];
+// let res = arr.filter(function (value) {
+//     return value > 0;
+// })
+// console.log(res);
+
+
+// 6chi masala
+// let arr = [13, 25, 32, 6, 12];
+// let res = arr.filter(function (value) {
+//     return value % 2 == 0;
+// })
+// console.log(res);
+
+
+// 7chi masala
+// let arr = ['salom', 'hello', 'bye', 'thenkyou',];
+// let res = arr.filter(function (value) {
+//     return value.length < 5;
+// })
+// console.log(res);
+
+
+// 8chi masala
+// let users = [
+//     { ism: "ali", yosh: 20 },
+//     { ism: "odil", yosh: 25 },
+//     { ism: "rasil", yosh: 16 },
+//     { ism: "tillo", yosh: 30 }
+// ];
+// let res = users.filter(function (value) {
+//     return value.yosh > 18;
+// }).map(function (value) {
+//     return value.yosh;
+// });
+// console.log(res);
+
+
+// 9chi masala
+// let arr = [1, 2, 3, 4, 5];
+// let res = arr.map(function (value) {
+//     return value  2
+// })
+// console.log(res);
+
+
+// 10chi masala
+// let arr = [1, 2, 3, 4, 12];
+// let res = arr.map(function (value) {
+//     return value + 5
+// })
+// console.log(res);
+
+
+// 11chi masala
+// let arr = ['salom', 'hello', 'bye'];
+// let res = arr.map(function (value) {
+//     return value.toUpperCase()
+// })
+// console.log(res);
+
+
+// 14chi masala
+// let arr = ['salom', 'hello'];
+// let res = arr.some(function (value) {
+//     return value.length > 10;
+// })
+// console.log(res);
+
+
+// 15chi masala
+// let arr = [1, 2, 3, 4, 12];
+// let res = arr.every(function (value) {
+//     return value > 0;
+// })
+// console.log(res);
+
+
+// 16chi masala
+// let arr = ['salom', 'hello'];
+// let res = arr.every(function (value) {
+//     return value == value.toUpperCase;
+// })
+// console.log(res);
+
+
+// 17chi masala
+// let arr = [1, 2, 3, 4, 5];
+// let res = arr.includes(13);
+// console.log(res);
+
+
+// 18chi masala
+// : Berilgan massivda ma'lum bir elementning birinchi
+// uchragan indeksini toping va qaytaring.
+// let arr = [14, 12, 18, 46, 17];
+// let res = arr.indexOf(18);
+// console.log(res);
+
+
+// 19chi masala
+// let arr = [13, 12, 13, 4, 12];
+// let res = arr.lastIndexOf(46);
+// console.log(res);
+
+
+// 20chi masala
+// let arr = [13, 12, 18, 16, 12];
+// let res = arr.find(function (value) {
+//     return value > 10 && value % 2 == 0
+// })
+// console.log(res);
+
+
+// 21chi masala
+// let arr = [14, 12, 19, 45, 12];
+// let res = arr.findIndex(function (value) {
+//     return value > 10 && value % 2 == 0
+// })
+// console.log(res);
+
+
+// 22chi masala
+// let arr = [10, 12, 17, 38, 12];
+// let res = arr.findLast(function (value) {
+//     return value > 10 && value % 2 == 0
+// })
+// console.log(res);
+
+
+// 23chi masala
+// let arr = [15, 10, 13, 58, 32];
+// let res = arr.findLastIndex(function (value) {
+//     return value > 10 && value % 2 == 0
+// })
+// console.log(res);
+
+
+// 24chi masala
+// let arr = [13, 12, 13, 26, 12];
+// let arr2 = arr.reverse();
+// console.log(arr2);
+
+
+// 25chi masala
+// let arr = [23, 17, 33, 41, 12];
+// let res = arr.sort(function (a, b) {
+//     return a - b
+// })
+// console.log(res);
+
+
+// 26chi masala
+// let arr = [14, 22, 13, 56, 22];
+// let res = arr.forEach(function (value) {
+//     value = value * 2
+//     console.log(value);
+// })
+
+// 27chi masala
+// let arr = [23, 42, -13, 26, 12];
+// let res = arr.filter(function (value) {
+//     return value > 0
+// })
+// console.log(res);
+
+// 28chi masala
+// let arr = [7, 12, 23, 4, 10];
+// let res = arr.map(function (value) {
+//     return value  2;
+// })
+// console.log(res);
+
+
+// 29chi masala
+// let arr = [13 ,10, -19, 26, -12];
+// let res = arr.some(function (value) {
+//     return value < 0
+// })
+// console.log(res);
+
+// 30chi masala
+// let arr = [10, 12, 13, 72];
+// let res = arr.every(function (value) {
+//     return value > 0
+// })
+// console.log(res);
+
+
+// 31chi masala
+// let arr = [13, 12, 23, 22];
+// let res = arr.filter(function (value) {
+//     return value > 10
+// })
+// console.log(res);
+
+
+// 32chi masala
+// let arr = [13, 12,  16, 22];
+// let res = arr.map(function (value) {
+//     return value > 10
+// })
+// console.log(res);
